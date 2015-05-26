c66d317cf8d6eba4bdb3ba30ffe91bea94e934ec
dominik791 <dominik.broj@gmail.com>
2015-05-26 19:42:08 +0200

    checking git settings

--- RunAway.py
+++ RunAway.py
@@ -231,8 +231,7 @@ class Map(object):
     def opening_scene(self):
     	
         return self.next_scene(self.start_scene)
-   
-   
+      
 a_map = Map("start")
 a_game = Engine(a_map)
 a_game.play()
